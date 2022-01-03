e56b3ad447ff9317483a46ce58fcf9aabe6fbe1c
Alex Brouwer <alex@factorit.nl>
2022-01-03 13:48:45 +0000

    prepare release

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -25,6 +25,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 //    ]);
 
     $parameters->set(Option::PACKAGE_ALIAS_FORMAT, '<major>.<minor>.x-dev');
+    $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
 
     $services = $containerConfigurator->services();
 
